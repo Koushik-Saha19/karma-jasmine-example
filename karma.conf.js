7652556d37487ad4204d89b10f34f54f5846ce16
Koushik-Saha19 <55374424+Koushik-Saha19@users.noreply.github.com>
2020-07-10 01:10:26 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -31,55 +31,23 @@ module.exports = function(config) {
 
 
     // list of files to exclude
-    exclude: [
+   exclude: [
+      // Exclude main module because it would interfere with the test-init module.
+      'src/main.ts'
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    proxies: {
+      // Map requests to sources to the base path where Karma serves them.
+      '/src/': '/base/src/'
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-
-    // web server port
+    preprocessors: {
+      // Pass all TypeScript sources to the preprocessor.
+      '**/*.ts': ['karma-typescript']
+    },
+    reporters: ['progress', 'karma-typescript'],
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-   
-//     browserDisconnectTimeout: 10000,
-//     browserDisconnectTolerance: 3,
-//     captureTimeout: 60000,
-//     browserNoActivityTimeout: 360000,
-//     flags: [
-//       '--disable-web-security',
-//       '--disable-gpu',
-//       '--no-sandbox'
-//     ],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    autoWatch: false,
     singleRun: true
   })
 }
